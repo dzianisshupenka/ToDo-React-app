621fec666bc26c519035b14baa4a57c6f0a0b47c
Dzianis <imxrayz@gmail.com>
2020-05-09 11:25:39 +0300

    Updates

---
+++ precache-manifest.2b07afb3284302a8fa650fd60b2df74c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e251f8d9d1a6b932f2259e61bf24c358",
+    "url": "/ToDo-React-app/index.html"
+  },
+  {
+    "revision": "f78ae6ed2c8e35aecb66",
+    "url": "/ToDo-React-app/static/css/main.7a566ee3.chunk.css"
+  },
+  {
+    "revision": "8c8be03abacfdda0a87a",
+    "url": "/ToDo-React-app/static/js/2.f899221b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-React-app/static/js/2.f899221b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78ae6ed2c8e35aecb66",
+    "url": "/ToDo-React-app/static/js/main.32e5bd7b.chunk.js"
+  },
+  {
+    "revision": "ee4d018633aba2abf8e2",
+    "url": "/ToDo-React-app/static/js/runtime-main.f8674edb.js"
+  }
+]);
